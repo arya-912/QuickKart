5fdd0ea1f6de5a49954db8fd348a1efbe661d586
arya-912 <pravesharya2004@gmail.com>
2025-05-18 13:12:31 +0530

    Updated package.json

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,13 @@
+// import { redis } from '../lib/redis.js'
 import User from '../models/user.model.js'
+import jwt from 'jsonwebtoken'
+
+const generateToken = (userId) => {
+	const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+		expiresIn: '15m'
+	})
+}
+
 
 export const signup = async (req, res) => {
 	const { email, password, name } = req.body
@@ -11,8 +20,20 @@ export const signup = async (req, res) => {
 		}
 		const user = await User.create({ name, email, password })
 
-		res.status(201).json({ user, message: "User created successfully" })
+		// authenticate
+		const { accessToken, refreshToken}  = generateToken(user._id)
+		await storeRefreshToken(user._id, refreshToken)
+
+		setCookies(res, accessToken, refreshToken)
+
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			role: user.role,
+		})
 	} catch (error) {
+		console.log("Error in signup controller", error.message)
 		res.status(500).json({ message: error.message })
 	}
 }
